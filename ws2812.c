d110e079ab739afc02ef3501921ee3c16d838f7f
Isaque Coelho <isaquecoelho.dj@gmail.com>
2025-02-02 22:51:26 -0300

    identatação

--- ws2812.c
+++ ws2812.c
@@ -21,69 +21,68 @@ uint sm;
 
 // Padrões dos números (0-9) na matriz 5x5
 double desenho1[25] = {0.0, 0.0, 0.2, 0.0, 0.0,
-                        0.0, 0.0, 0.2, 0.2, 0.0, 
-                        0.0, 0.0, 0.2, 0.0, 0.0,
-                        0.0, 0.0, 0.2, 0.0, 0.0,
-                        0.0, 0.2, 0.2, 0.2, 0.0};
+                       0.0, 0.0, 0.2, 0.2, 0.0,
+                       0.0, 0.0, 0.2, 0.0, 0.0,
+                       0.0, 0.0, 0.2, 0.0, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0};
 
 double desenho2[25] = {0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.0, 0.0, 
-                        0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.0, 0.0, 0.2, 0.0,
-                        0.0, 0.2, 0.2, 0.2, 0.0};
+                       0.0, 0.2, 0.0, 0.0, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0,
+                       0.0, 0.0, 0.0, 0.2, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0};
 
 double desenho3[25] = {0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.0, 0.0, 
-                        0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.0, 0.0,
-                        0.0, 0.2, 0.2, 0.2, 0.0};
+                       0.0, 0.2, 0.0, 0.0, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0,
+                       0.0, 0.2, 0.0, 0.0, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0};
 
 double desenho4[25] = {0.0, 0.2, 0.0, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.2, 0.0, 
-                        0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.0, 0.0,
-                        0.0, 0.0, 0.0, 0.2, 0.0};
+                       0.0, 0.2, 0.0, 0.2, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0,
+                       0.0, 0.2, 0.0, 0.0, 0.0,
+                       0.0, 0.0, 0.0, 0.2, 0.0};
 
 double desenho5[25] = {0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.0, 0.0, 0.2, 0.0, 
-                        0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.0, 0.0,
-                        0.0, 0.2, 0.2, 0.2, 0.0};
+                       0.0, 0.0, 0.0, 0.2, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0,
+                       0.0, 0.2, 0.0, 0.0, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0};
 
 double desenho6[25] = {0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.0, 0.0, 0.2, 0.0, 
-                        0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.2, 0.0,
-                        0.0, 0.2, 0.2, 0.2, 0.0};
+                       0.0, 0.0, 0.0, 0.2, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0,
+                       0.0, 0.2, 0.0, 0.2, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0};
 
-
-double desenho7[25] = { 0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.0, 0.0, 
-                        0.0, 0.0, 0.2, 0.0, 0.0,
-                        0.0, 0.0, 0.0, 0.2, 0.0,
-                        0.2, 0.0, 0.0, 0.0, 0.0};
+double desenho7[25] = {0.0, 0.2, 0.2, 0.2, 0.0,
+                       0.0, 0.2, 0.0, 0.0, 0.0,
+                       0.0, 0.0, 0.2, 0.0, 0.0,
+                       0.0, 0.0, 0.0, 0.2, 0.0,
+                       0.2, 0.0, 0.0, 0.0, 0.0};
 
 double desenho8[25] = {0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.2, 0.0, 
-                        0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.2, 0.0,
-                        0.0, 0.2, 0.2, 0.2, 0.0};
+                       0.0, 0.2, 0.0, 0.2, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0,
+                       0.0, 0.2, 0.0, 0.2, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0};
 
 double desenho9[25] = {0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.2, 0.0, 
-                        0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.0, 0.0,
-                        0.0, 0.2, 0.2, 0.2, 0.0};
+                       0.0, 0.2, 0.0, 0.2, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0,
+                       0.0, 0.2, 0.0, 0.0, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0};
 
 double desenho0[25] = {0.0, 0.2, 0.2, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.2, 0.0, 
-                        0.0, 0.2, 0.0, 0.2, 0.0,
-                        0.0, 0.2, 0.0, 0.2, 0.0,
-                        0.0, 0.2, 0.2, 0.2, 0.0};
-
+                       0.0, 0.2, 0.0, 0.2, 0.0,
+                       0.0, 0.2, 0.0, 0.2, 0.0,
+                       0.0, 0.2, 0.0, 0.2, 0.0,
+                       0.0, 0.2, 0.2, 0.2, 0.0};
 
 // Função para converter valores de cor para formato WS2812
-uint32_t matrix_rgb(double brilho, double r, double g, double b) {
+uint32_t matrix_rgb(double brilho, double r, double g, double b)
+{
     uint8_t R = (uint8_t)(r * brilho * 255);
     uint8_t G = (uint8_t)(g * brilho * 255);
     uint8_t B = (uint8_t)(b * brilho * 255);
@@ -92,52 +91,85 @@ uint32_t matrix_rgb(double brilho, double r, double g, double b) {
 }
 
 // Função para enviar padrão para a matriz de LEDs
-void desenho_pio1(double *desenho) {
-    for (int i = 0; i < NUM_PIXELS; i++) {
+void desenho_pio1(double *desenho)
+{
+    for (int i = 0; i < NUM_PIXELS; i++)
+    {
         uint32_t valor_led = matrix_rgb(desenho[24 - i], r, g, b);
         pio_sm_put_blocking(pio, sm, valor_led);
     }
 }
 // Configuração inicial dos LEDs WS2812
-void setup_ws2812() {
-    if (!set_sys_clock_khz(128000, false)) {
+void setup_ws2812()
+{
+    if (!set_sys_clock_khz(128000, false))
+    {
         printf("Erro ao configurar o clock!\n");
-        while(1);
+        while (1)
+            ;
     }
     uint offset = pio_add_program(pio, &ws2812_program);
     sm = pio_claim_unused_sm(pio, true);
     ws2812_program_init(pio, sm, offset, WS2812_PIN, 800000, false);
 }
 // Seleciona padrão correspondente ao número atual
-void display_number(int number) {
-    switch(number) {
-        case 0: desenho_pio1(desenho0); break;
-        case 1: desenho_pio1(desenho1); break;
-        case 2: desenho_pio1(desenho2); break;
-        case 3: desenho_pio1(desenho3); break;
-        case 4: desenho_pio1(desenho4); break;
-        case 5: desenho_pio1(desenho5); break;
-        case 6: desenho_pio1(desenho6); break;
-        case 7: desenho_pio1(desenho7); break;
-        case 8: desenho_pio1(desenho8); break;
-        case 9: desenho_pio1(desenho9); break;
-        default: break;
+void display_number(int number)
+{
+    switch (number)
+    {
+    case 0:
+        desenho_pio1(desenho0);
+        break;
+    case 1:
+        desenho_pio1(desenho1);
+        break;
+    case 2:
+        desenho_pio1(desenho2);
+        break;
+    case 3:
+        desenho_pio1(desenho3);
+        break;
+    case 4:
+        desenho_pio1(desenho4);
+        break;
+    case 5:
+        desenho_pio1(desenho5);
+        break;
+    case 6:
+        desenho_pio1(desenho6);
+        break;
+    case 7:
+        desenho_pio1(desenho7);
+        break;
+    case 8:
+        desenho_pio1(desenho8);
+        break;
+    case 9:
+        desenho_pio1(desenho9);
+        break;
+    default:
+        break;
     }
 }
 // Manipulador de interrupção para os botões
-void gpio_irq_handler(uint gpio, uint32_t events) {
+void gpio_irq_handler(uint gpio, uint32_t events)
+{
     uint32_t current_time = to_ms_since_boot(get_absolute_time());
-        // Tratamento para botão A (incremento)
-    if (gpio == PIN_BUTTON_A) {
-        if (current_time - last_time_A > DEBOUNCE_TIME_MS && contador < 9) {
+    // Tratamento para botão A (incremento)
+    if (gpio == PIN_BUTTON_A)
+    {
+        if (current_time - last_time_A >= DEBOUNCE_TIME_MS && contador < 9)
+        {
             last_time_A = current_time;
             contador++;
             display_number(contador);
         }
-    } 
-        // Tratamento para botão B (decremento
-    else if (gpio == PIN_BUTTON_B) {
-        if (current_time - last_time_B > DEBOUNCE_TIME_MS && contador > 0) {
+    }
+    // Tratamento para botão B (decremento
+    else if (gpio == PIN_BUTTON_B)
+    {
+        if (current_time - last_time_B > DEBOUNCE_TIME_MS && contador > 0)
+        {
             last_time_B = current_time;
             contador--;
             display_number(contador);
@@ -145,12 +177,13 @@ void gpio_irq_handler(uint gpio, uint32_t events) {
     }
 }
 
-int main() {
+int main()
+{
     stdio_init_all();
     // Configura LED vermelho
     gpio_init(PIN_LED_RED);
     gpio_set_dir(PIN_LED_RED, GPIO_OUT);
-  // Configura botões com pull-up interno
+    // Configura botões com pull-up interno
     gpio_init(PIN_BUTTON_A);
     gpio_set_dir(PIN_BUTTON_A, GPIO_IN);
     gpio_pull_up(PIN_BUTTON_A);
@@ -166,9 +199,11 @@ int main() {
     display_number(0);
     // Loop principal para piscar LED vermelho
     uint32_t last_toggle = 0;
-    while (true) {
+    while (true)
+    {
         uint32_t current_time = to_ms_since_boot(get_absolute_time());
-        if (current_time - last_toggle >= 100) {
+        if (current_time - last_toggle >= 100)
+        {
             gpio_put(PIN_LED_RED, !gpio_get(PIN_LED_RED));
             last_toggle = current_time;
         }
